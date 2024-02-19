1d0aee8bd218188a1ed8c3e14376c0980899bbb7
Bdud1 <bdudman94@gmail.com>
2024-02-19 21:45:16 +0100

    Creates check_guess and ask_for_input functions to clean up code.

--- milestone_3.py
+++ milestone_3.py
@@ -9,16 +9,24 @@ word_list = ["mango", "strawberry", "peach", "passion fruit", "pineapple"]
 word = random.choice(word_list)
 print(word)
 
-# Request user to input a single letter
-guess = input("Please select a letter: ")
-while True:
-    if len(guess) == 1 and guess.isalpha():
-        break
+## Check whether the guessed letter is in the word
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        guess = input("Invalid letter. Please, enter a single alphabetical character: ")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
- 
+# Request user to input a single letter
+def ask_for_input():
+    guess = input("Please select a letter: ")
+    while True:
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            guess = input("Invalid letter. Please, enter a single alphabetical character: ")
+    check_guess(guess)    
+
+
+
+ask_for_input()
