feeedc2b34b07e86a98a59775d8aaaf91a219531
Bdud1 <bdudman94@gmail.com>
2024-02-19 21:29:50 +0100

    Adds if/else block to check if guessed letter is in word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,14 @@
+## Import random module
+import random 
+
+## Initialise list of favourite fruits and assign to word_list
+word_list = ["mango", "strawberry", "peach", "passion fruit", "pineapple"]
+# print(word_list)
+
+## Select a random fruit from word_list
+word = random.choice(word_list)
+print(word)
+
 # Request user to input a single letter
 guess = input("Please select a letter: ")
 while True:
@@ -6,5 +17,8 @@ while True:
     else:
         guess = input("Invalid letter. Please, enter a single alphabetical character: ")
 
-
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
  
