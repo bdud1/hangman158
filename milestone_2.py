09e78037950d414ac5563bf8eb00d290a2725752
Bradley Dudman <brad@Bradleys-Air.lan>
2024-02-19 21:11:29 +0100

    Adds an if/else statement to check user input is valid in length and type

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,17 @@
+## Import random module
 import random 
 
+## Initialise list of favourite fruits and assign to word_list
 word_list = ["mango", "strawberry", "peach", "passion fruit", "pineapple"]
-print(word_list)
+# print(word_list)
 
-print(random.choice(word_list))
+## Select a random fruit from word_list
+word = print(random.choice(word_list))
+# print(word)
+
+## Request user to input a single letter
+guess = input("Please select a letter: ")
+
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
