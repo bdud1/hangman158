6be1d192da56ec91cb992355c613dcf2d70cd20d
Bdud1 <bdudman94@gmail.com>
2024-02-20 10:45:33 +0100

    Builds check_guess method to replace indexed letter if guess is correct and reduces a life if guess is incorrect.

--- milestone_4.py
+++ milestone_4.py
@@ -22,6 +22,17 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for letter in self.word:
+                if letter == guess:
+                    index_letter = self.word.index(letter) 
+                    self.word_guessed[index_letter] = guess
+                    print(self.word_guessed)
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left")
+
 
     # Request user to input a single letter
     def ask_for_input(self):  
